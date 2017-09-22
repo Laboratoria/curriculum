d14b2e442e9f7f795f76a5024057ca7308da0798
Alexander <aocsa.cs@gmail.com>
2017-09-22 12:43:53 -0500

    update & revision: first 15 code-challenges. V.2

---
+++ 02-basic-programming/01-basic-programming/13-code-challenges/13-get-longest-word-of-mixed-elements/test/getLongestWordOfMixedElements.spec.js
@@ -0,0 +1,24 @@
+
+const Assert = require('chai').assert;
+const getLongestWordOfMixedElements = require('../solution/getLongestWordOfMixedElements');
+
+describe("getLongestWordOfMixedElements()", function () {
+  it("deberia regresar '', para  ([3, 4, 5, 6, 7])", function () {
+	var output = getLongestWordOfMixedElements([3, 4, 5, 6, 7]);
+	Assert.deepEqual(output, '');
+  });
+
+  it("deberia regresar '', para ([])", function () {
+	var output = getLongestWordOfMixedElements([]);
+	Assert.deepEqual(output, '');
+  });
+  
+
+  it("deberia regresar 'word', para ([3, 'word', 5, 'up', 3, 1])", function () {
+
+	var output = getLongestWordOfMixedElements([3, 'word', 5, 'up', 3, 1]);
+	Assert.deepEqual(output, 'word');
+
+  });
+ 
+});
---
+++ 02-basic-programming/01-basic-programming/13-code-challenges/14-get-largest-number-among-mixed-elements/test/getLargestNumberAmongMixedElements.spec.js
@@ -0,0 +1,23 @@
+
+const Assert = require('chai').assert;
+const getLargestNumberAmongMixedElements = require('../solution/getLargestNumberAmongMixedElements');
+
+describe("getLargestNumberAmongMixedElements()", function () {
+  it("deberia regresar 0, para  (['wat', 'no', true, true])", function () {
+	var output = getLargestNumberAmongMixedElements(['wat', 'no', true, true]);
+	Assert.deepEqual(output, 0);
+  });
+
+  it("deberia regresar 0, para ([])", function () {
+	var output = getLargestNumberAmongMixedElements([]);
+	Assert.deepEqual(output, 0);
+  });
+  
+
+  it("deberia regresar 5, para ([3, 'word', 5, 'up', 3, 1])", function () {
+	var output = getLargestNumberAmongMixedElements([3, 'word', 5, 'up', 3, 1]);
+	Assert.deepEqual(output, 5);
+
+  });
+ 
+});
