3a98eabf43035d435e5f59353c94d2aec0c421cd
Nicole Stein <nicolethenerd@gmail.com>
2017-10-30 11:40:53 -0500

    chore(99-general-concepts): rename folders and add one more code challenge

---
+++ 99-general-concepts/01-debugging/03-code-challenges/03-calculate-tip/README.md
@@ -0,0 +1,11 @@
+# ReferenceError: ___ not defined
+
+
+Corrije los errores en la función y explique cuál era el problema en un
+comentario.
+
+Ejemplo de entrada:
+
+```js
+
+```
---
+++ 99-general-concepts/01-debugging/03-code-challenges/03-calculate-tip/boilerplate/calculateTip.js
@@ -0,0 +1,19 @@
+/* Retorna num1 por ciento de num2. */
+function percent(num1, num2) {
+  return num1 / 100 * num2;
+}
+
+function calculateTip(billAmount) {
+  var percent = percent(15, billAmount);
+  return percent;
+}
+
+calculateTip(100);
+
+/**
+ * ¿Cuál fue el problema con el código?
+ *
+ * ESCRIBA SU RESPUESTA AQUÍ.
+ */
+
+module.exports = calculateTip;
