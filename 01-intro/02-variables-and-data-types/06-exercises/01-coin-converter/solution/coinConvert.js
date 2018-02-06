231239d0d6c10b35d69f22b61e9a18b548f1e83a
MaiaRojas <maia.rt.46@gmail.com>
2018-02-06 16:16:52 -0500

    content(01-intro): deleted content exercises

--- 01-intro/02-variables-and-data-types/06-exercises/01-coin-converter/solution/coinConvert.js
+++ 01-intro/02-variables-and-data-types/06-exercises/01-coin-converter/solution/coinConvert.js
@@ -1,18 +1,3 @@
 module.exports = function coinConverter() {
 
-  var usDollars = 100;
-  var peruvianSoles = usDollars*3.25;
-  var mexicanPesos = usDollars*18;
-  var chileanPesos = usDollars*660;
-
-  return {
-    // Imprime el monto equivalente en soles
-    soles :() => console.log(peruvianSoles);
-
-    // Imprime el monto equivalente en pesos mexicanos
-    pesosMx:() => console.log(mexicanPesos);
-
-    // Imprime el monto equivalente en pesos chilenos
-    pesosCl:() => console.log(chileanPesos);
-  };
 };
--- 01-intro/02-variables-and-data-types/06-exercises/02-restaurant-bill/test/restaurantBill.spec.js
+++ 01-intro/02-variables-and-data-types/06-exercises/02-restaurant-bill/test/restaurantBill.spec.js
@@ -1,18 +1,7 @@
 
 const Assert = require('chai').assert;
-const computeAverageLengthOfWords = require('../solution/computeAverageLengthOfWords');
+const restautantBill = require('../solution/restautantBill');
 
-describe("computeAverageLengthOfWords()", function () {
-  it("debería regresar  4, para  ('A', 'kamnjhs')", function () {Ejemplo: promedio = 4
-    Assert.deepEqual(computeAverageLengthOfWords("A", "kamnjhs"), 4);
-  });
-
-  it("debería regresar  12, para ('Lalalalala', 'Kekekekekekeke')", function () {
-    Assert.deepEqual(computeAverageLengthOfWords("Lalalalala", "Kekekekekekeke"),  12);
-  });
-
-  it("debería regresar 5, para ('Kemal', 'Kemal')", function () {
-    Assert.deepEqual(  computeAverageLengthOfWords("Kemal", "Kemal"),  5);
-  });
+describe("restaurantBill()", function () {
 
 });
