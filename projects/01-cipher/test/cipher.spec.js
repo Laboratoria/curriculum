d69f3f5c5256fb6f77ba966d93c16d39f04317fb
Lupo Montero <lupomontero@gmail.com>
2018-05-22 14:18:05 -0500

    projects(01-cipher): clarifica hacker edition y requisitos generales
    * mueve `cipher.createCipherWithOffset` a parte obligatoria
    * agrega minúsculas y caracteres no alfabéticos a hacker edition
    * agrega offset negativo a hacker edition
    * comenta tests de hacker edition en boilerplate (opcional)
    
    Related:
    
    * https://github.com/Laboratoria/curricula-js/pull/582

--- projects/01-cipher/test/cipher.spec.js
+++ projects/01-cipher/test/cipher.spec.js
@@ -17,13 +17,25 @@ describe('cipher', () => {
       );
     });
 
-    it('debería retornar "hijklmnopqrstuvwxyzabcdefg" para "abcdefghijklmnopqrstuvwxyz" con offset 33', () => {
-      assert.equal(cipher.encode(33, 'abcdefghijklmnopqrstuvwxyz'), 'hijklmnopqrstuvwxyzabcdefg');
-    });
+    //
+    // Hacker edition
+    //
+    // Si decides agregar soporte para minúsculas descomenta el test a
+    // continuación.
+    //
+    // it('debería retornar "hijklmnopqrstuvwxyzabcdefg" para "abcdefghijklmnopqrstuvwxyz" con offset 33', () => {
+    //   assert.equal(cipher.encode(33, 'abcdefghijklmnopqrstuvwxyz'), 'hijklmnopqrstuvwxyzabcdefg');
+    // });
 
-    it('debería retornar " !@" para " !@"', () => {
-      assert.equal(cipher.encode(33, ' !@'),' !@');
-    });
+    //
+    // Hacker edition
+    //
+    // Si decides implementar soporte para caracteres no alfabéticos descomenta
+    // el test a continuación.
+    //
+    // it('debería retornar " !@" para " !@"', () => {
+    //   assert.equal(cipher.encode(33, ' !@'),' !@');
+    // });
 
   });
 
@@ -40,12 +52,18 @@ describe('cipher', () => {
       );
     });
 
-    it('debería retornar "abcdefghijklmnopqrstuvwxyz" para "hijklmnopqrstuvwxyzabcdefg" con offset 33', () => {
-      assert.equal(
-        cipher.decode(33, 'hijklmnopqrstuvwxyzabcdefg'),
-        'abcdefghijklmnopqrstuvwxyz'
-      );
-    });
+    //
+    // Hacker edition
+    //
+    // Si decides agregar soporte para minúsculas descomenta el test a
+    // continuación.
+    //
+    // it('debería retornar "abcdefghijklmnopqrstuvwxyz" para "hijklmnopqrstuvwxyzabcdefg" con offset 33', () => {
+    //   assert.equal(
+    //     cipher.decode(33, 'hijklmnopqrstuvwxyzabcdefg'),
+    //     'abcdefghijklmnopqrstuvwxyz'
+    //   );
+    // });
   });
 
   describe('cipher.createCipherWithOffset', () => {
@@ -69,9 +87,15 @@ describe('cipher', () => {
         assert.equal(cipherWithOffset.encode('ABCD'), 'HIJK');
       });
 
-      it('debería retornar "hijk" para "abcd"', () => {
-        assert.equal(cipherWithOffset.encode('abcd'), 'hijk');
-      });
+      //
+      // Hacker edition
+      //
+      // Si decides agregar soporte para minúsculas descomenta el test a
+      // continuación.
+      //
+      // it('debería retornar "hijk" para "abcd"', () => {
+      //   assert.equal(cipherWithOffset.encode('abcd'), 'hijk');
+      // });
 
     });
 
@@ -83,9 +107,15 @@ describe('cipher', () => {
         assert.equal(cipherWithOffset.decode('HIJK'), 'ABCD');
       });
 
-      it('debería retornar "abcd" para "hijk"', () => {
-        assert.equal(cipherWithOffset.encode('abcd'), 'hijk');
-      });
+      //
+      // Hacker edition
+      //
+      // Si decides agregar soporte para minúsculas descomenta el test a
+      // continuación.
+      //
+      // it('debería retornar "abcd" para "hijk"', () => {
+      //   assert.equal(cipherWithOffset.encode('abcd'), 'hijk');
+      // });
 
     });
 
