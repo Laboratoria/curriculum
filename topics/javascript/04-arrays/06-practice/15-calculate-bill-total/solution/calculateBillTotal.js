fd50a8b6379d2a73d60358b618d5a08ed58b5eeb
AnaSalazar <anastephany.salazarguzman@gmail.com>
2018-06-01 16:03:28 -0500

    Soluciona nuevos errores marcados por Travis

--- topics/javascript/04-arrays/06-practice/15-calculate-bill-total/solution/calculateBillTotal.js
+++ topics/javascript/04-arrays/06-practice/15-calculate-bill-total/solution/calculateBillTotal.js
@@ -1,3 +1,6 @@
-const calculateBillTotal = (preTaxAndTipAmount) => preTaxAndTipAmount + (preTaxAndTipAmount * 0.095) + (preTaxAndTipAmount * 0.15);
+const calculateBillTotal = (preTaxAndTipAmount) => {
+  var result = preTaxAndTipAmount + (preTaxAndTipAmount * 0.095) + (preTaxAndTipAmount * 0.15);
+  return result;
+}
 
 module.exports = calculateBillTotal;
