9480daf59283196a1f9044500b4e9da922b12393
Fabian Alexis Bravo Abarca <fabian.bravo.abarca@gmail.com>
2018-10-10 17:30:58 +0000

    Agrega comentarios y algunos detalles a proyecto bq-node (#728)

--- projects/05-bq-node/routes/users.js
+++ projects/05-bq-node/routes/users.js
@@ -37,7 +37,39 @@ const initAdminUser = (app, next) => {
 };
 
 
+/*
+ * Diagrama de flujo de una aplicación y petición en node - express : 
+ * 
+ * request  -> middleware1 -> middleware2 -> route
+ *                                             | 
+ * response <- middleware4 <- middleware3   <---
+ * 
+ * la gracia es que la petición va pasando por cada una de las funciones 
+ * intermedias o "middlewares" hasta llegar a la función de la ruta, luego esa
+ * función genera la respuesta y esta pasa nuevamente por otras funciones 
+ * intermedias hasta responder finalmente al usuario.
+ * 
+ * Un ejemplo de middleware podría ser una función que verifique que un usuario 
+ * está realmente registrado en la aplicación y que tiene permisos para usar la
+ * ruta. O también un middleware de traducción, que cambie la respuesta 
+ * dependiendo del idioma del usuario.
+ * 
+ * Es por lo anterior que siempre veremos los argumentos request, response y
+ * next en nuestros middlewares y rutas. Cada una de estas funciones tendrá
+ * la oportunidad de acceder a la consulta (request) y hacerse cargo de enviar
+ * una respuesta (rompiendo la cadena), o delegar la consulta a la siguiente
+ * función en la cadena (invocando next). De esta forma, la petición (request)
+ * va pasando a través de las funciones, así como también la respuesta
+ * (response).
+ */
+
+
 module.exports = (app, next) => {
+  /*
+   * Cuando la ruta llamada tenga entre sus parámetros uid, ejecutará la función
+   * que cargará el usuario desde la base de datos, dejándo disponible esa 
+   * Información para las rutas que están acá.
+   */
   app.param('uid', (req, res, next, uid) => {
     const { auth } = req;
 
