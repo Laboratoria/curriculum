6c2097c46176ba8c0c1304f587b87853ec855316
developervilchez <developervilchez@gmail.com>
2018-10-15 14:06:38 -0500

    limpia console log y aplica guia de estilo

--- 01-intro/04-flow-control/06-code-challenges/02-reverse/test/parse.js
+++ 01-intro/04-flow-control/06-code-challenges/02-reverse/test/parse.js
@@ -1,13 +1,12 @@
 const parse = (str) => {
   const fs = require('fs');
   const path = require('path');
-  const filePath = path.join(__dirname, str);
-  console.log(filePath)
-  const buffer = fs.readFileSync(filePath, {
+  const filePath = path.join(__dirname , str);
+  const buffer = fs.readFileSync(filePath , {
     encoding: 'utf8'
   });
 
-  return buffer.indexOf('.reverse()')
+  return buffer.indexOf('.reverse()');
 }
 
 module.exports = parse
