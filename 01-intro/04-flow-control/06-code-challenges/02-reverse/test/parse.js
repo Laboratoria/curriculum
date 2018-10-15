7c52e3e7def8c4ea72f68ca843de689912748925
developervilchez <developervilchez@gmail.com>
2018-10-15 12:33:56 -0500

    agrega file parse para los retos 1 y 2, modifica los readme de los ejercicios

---
+++ 01-intro/04-flow-control/06-code-challenges/02-reverse/test/parse.js
@@ -0,0 +1,13 @@
+const parse = (str) => {
+  const fs = require('fs');
+  const path = require('path');
+  const filePath = path.join(__dirname, str);
+  console.log(filePath)
+  const buffer = fs.readFileSync(filePath , {
+    encoding: 'utf8'
+  });
+
+   return buffer.indexOf('.reverse()')
+}
+
+module.exports = parse
