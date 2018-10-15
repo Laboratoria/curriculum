f4945e0b9c8b4275bc2655b48615f8633c2f20c3
developervilchez <developervilchez@gmail.com>
2018-10-15 13:29:55 -0500

    aplica identación en archivos parse

--- 01-intro/04-flow-control/06-code-challenges/01-power-of-two/test/parse.js
+++ 01-intro/04-flow-control/06-code-challenges/01-power-of-two/test/parse.js
@@ -1,10 +1,10 @@
 const parse = (str) => {
- const fs = require('fs');
- const path = require('path');
- const filePath = path.join(__dirname, str);
- const buffer = fs.readFileSync(filePath , {
-   encoding: 'utf8'
- });
+  const fs = require('fs');
+  const path = require('path');
+  const filePath = path.join(__dirname, str);
+  const buffer = fs.readFileSync(filePath, {
+    encoding: 'utf8'
+  });
 
   return buffer.indexOf('Math.pow()')
 }
--- 01-intro/04-flow-control/06-code-challenges/02-reverse/test/parse.js
+++ 01-intro/04-flow-control/06-code-challenges/02-reverse/test/parse.js
@@ -3,11 +3,11 @@ const parse = (str) => {
   const path = require('path');
   const filePath = path.join(__dirname, str);
   console.log(filePath)
-  const buffer = fs.readFileSync(filePath , {
+  const buffer = fs.readFileSync(filePath, {
     encoding: 'utf8'
   });
 
-   return buffer.indexOf('.reverse()')
+  return buffer.indexOf('.reverse()')
 }
 
 module.exports = parse
