75b944975f275068613f430fdfca7a9716eb0b11
developervilchez <developervilchez@gmail.com>
2018-10-17 16:47:07 -0500

    agrega archivo parse reto 1

---
+++ topics/javascript/02-flow-control/06-code-challenges/01-power-of-two/solution/parse.js
@@ -0,0 +1,11 @@
+const parse = (str) => {
+ const fs = require('fs');
+ const path = require('path');
+ const filePath = path.join(__dirname, str);
+ console.log(filePath)
+ const buffer = fs.readFileSync(filePath , {
+   encoding: 'utf8'
+ });
+   return buffer.indexOf('Math.pow')
+}
+module.exports = parse
