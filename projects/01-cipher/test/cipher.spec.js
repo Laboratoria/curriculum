36f385cc705861d8e25ada6e0c0c9e43810e444c
Lupo Montero <lupomontero@gmail.com>
2019-01-10 09:27:11 -0500

    project(cipher): applies suggestions from code review by @juanjordan

--- projects/01-cipher/test/cipher.spec.js
+++ projects/01-cipher/test/cipher.spec.js
@@ -10,7 +10,7 @@ describe('cipher', () => {
       assert.equal(typeof cipher.encode, 'function');
     });
 
-    it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offest 33');
+    it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offset 33');
     //
     // Hacker edition
     //
@@ -38,7 +38,7 @@ describe('cipher', () => {
       assert.equal(typeof cipher.decode, 'function');
     });
 
-    it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offest 33');
+    it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offset 33');
 
     //
     // Hacker edition
