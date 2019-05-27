b6b100068171a111843df06533ac6c23b63751b8
Rafael Cerri <rafaelbcerri@gmail.com>
2019-05-27 14:24:37 -0300

    topics(preadmission): Create preadmission content

---
+++ topics/preadmission-content/04-js-basics/06-exercises/01-coin-convert/boilerplate/coinConvert.js
@@ -0,0 +1,14 @@
+module.exports = (usDollars) => {
+  const arr = [];
+
+  // Añade el monto equivalente en soles
+  arr.push(/* tu código aquí */);
+
+  // Añade el monto equivalente en pesos mexicanos
+  arr.push(/* tu código aquí */);
+
+  // Añade el monto equivalente en pesos chilenos
+  arr.push(/* tu código aquí */);
+
+  return arr;
+};
