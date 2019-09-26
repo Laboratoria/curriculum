f963c58670853ccd15e64ceb4f3406d7eb98e646
Lupo Montero <lupomontero@gmail.com>
2019-09-26 16:03:03 -0500

    content(cipher): Replaces "test()" with "it()" in tests for consistency across projects

--- projects/01-cipher/test/cipher.spec.js
+++ projects/01-cipher/test/cipher.spec.js
@@ -3,18 +3,18 @@ require('../src/cipher');
 
 describe('cipher', () => {
 
-  test('should be an object', () => {
+  it('should be an object', () => {
     expect(typeof cipher).toBe('object');
   });
 
   describe('cipher.encode', () => {
 
-    test('should be a function', () => {
+    it('should be a function', () => {
       expect(typeof cipher.encode).toBe('function');
     });
 
-    test('should return "HIJKLMNOPQRSTUVWXYZABCDEFG" for "ABCDEFGHIJKLMNOPQRSTUVWXYZ" with offset 33', () => {
-      expect(cipher.encode(33, "ABCDEFGHIJKLMNOPQRSTUVWXYZ")).toBe("HIJKLMNOPQRSTUVWXYZABCDEFG");
+    it('should return "HIJKLMNOPQRSTUVWXYZABCDEFG" for "ABCDEFGHIJKLMNOPQRSTUVWXYZ" with offset 33', () => {
+      expect(cipher.encode(33, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ')).toBe('HIJKLMNOPQRSTUVWXYZABCDEFG');
     });
 
     // Hacker edition
@@ -27,7 +27,7 @@ describe('cipher', () => {
     // Se quiser adicionar testes para letras minúsculas, descomente o teste
     // abaixo.
     //
-    // test('should return "hijklmnopqrstuvwxyzabcdefg" for "abcdefghijklmnopqrstuvwxyz" with offset 33', () => {
+    // it('should return "hijklmnopqrstuvwxyzabcdefg" for "abcdefghijklmnopqrstuvwxyz" with offset 33', () => {
     //   expect(cipher.encode(33, 'abcdefghijklmnopqrstuvwxyz')).toBe('hijklmnopqrstuvwxyzabcdefg');
     // });
 
@@ -41,19 +41,19 @@ describe('cipher', () => {
     // Se quiser adicionar testes para caracteres não alfabéticos, descomente o
     // teste abaixo.
     //
-    // test('should return " !@" for " !@"', () => {
+    // it('should return " !@" for " !@"', () => {
     //   expect(cipher.encode(33, ' !@')).toBe(' !@');
     // });
   });
 
   describe('cipher.decode', () => {
 
-    test('should be a function', () => {
+    it('should be a function', () => {
       expect(typeof cipher.decode).toBe('function');
     });
 
-    test('should return "ABCDEFGHIJKLMNOPQRSTUVWXYZ" for "HIJKLMNOPQRSTUVWXYZABCDEFG" with offset 33', () => {
-      expect(cipher.decode(33, "HIJKLMNOPQRSTUVWXYZABCDEFG")).toBe("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+    it('should return "ABCDEFGHIJKLMNOPQRSTUVWXYZ" for "HIJKLMNOPQRSTUVWXYZABCDEFG" with offset 33', () => {
+      expect(cipher.decode(33, 'HIJKLMNOPQRSTUVWXYZABCDEFG')).toBe('ABCDEFGHIJKLMNOPQRSTUVWXYZ');
     });
 
     //
@@ -67,7 +67,7 @@ describe('cipher', () => {
     // Se quiser adicionar testes para letras minúsculas, descomente o teste
     // abaixo.
     //
-    // test('should return "abcdefghijklmnopqrstuvwxyz" for "hijklmnopqrstuvwxyzabcdefg" with offset 33', () => {
+    // it('should return "abcdefghijklmnopqrstuvwxyz" for "hijklmnopqrstuvwxyzabcdefg" with offset 33', () => {
     //   expect(cipher.decode(33, 'hijklmnopqrstuvwxyzabcdefg')).toBe('abcdefghijklmnopqrstuvwxyz');
     // });
 
@@ -81,7 +81,7 @@ describe('cipher', () => {
     // Se quiser adicionar testes para caracteres não alfabéticos, descomente o
     // teste abaixo.
     //
-    // test('should return " !@" para " !@"', () => {
+    // it('should return " !@" para " !@"', () => {
     //   expect(cipher.decode(33, ' !@')).toBe(' !@');
     // });
   });
