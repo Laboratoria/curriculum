6feed215a048dbb7c7cb71b252055284f9dd021d
Rafael Cerri <rafaelbcerri@gmail.com>
2019-10-04 18:12:02 -0300

    Update pt-BR translation

--- projects/01-cipher/test/cipher.spec.js
+++ projects/01-cipher/test/cipher.spec.js
@@ -1,4 +1,9 @@
-// importamos el objeto `cipher`, que contiene los métodos `encode` y `decode`
+// [Español]
+// Importamos el objeto `cipher`, que contiene los métodos `encode` y `decode`
+//
+// [Português]
+// Importamos o objeto `cipher`, que contém os métodos `encode` e `decode`
+
 import cipher from '../src/cipher';
 
 describe('cipher', () => {
