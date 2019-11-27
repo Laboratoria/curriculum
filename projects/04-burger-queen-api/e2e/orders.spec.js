4abe3241c836a7e86b115ead24da3480f2fc95f3
merunga <merunga@gmail.com>
2019-11-27 16:57:59 -0500

    project(bq-api): POST orders test client required

--- projects/04-burger-queen-api/e2e/orders.spec.js
+++ projects/04-burger-queen-api/e2e/orders.spec.js
@@ -41,7 +41,7 @@ describe('POST /orders', () => {
       })
       .then(([product, user]) => fetchAsTestUser('/orders', {
         method: 'POST',
-        body: { products: [{ productId: product._id, qty: 5 }], userId: user._id },
+        body: { products: [{ productId: product._id, qty: 5, client: 'client' }], userId: user._id },
       }))
       .then((resp) => {
         expect(resp.status).toBe(200);
@@ -49,6 +49,7 @@ describe('POST /orders', () => {
       })
       .then((json) => {
         expect(typeof json._id).toBe('string');
+        expect(json.client).toBe('client');
         expect(typeof json.dateEntry).toBe('string');
         expect(Array.isArray(json.products)).toBe(true);
         expect(json.products.length).toBe(1);
