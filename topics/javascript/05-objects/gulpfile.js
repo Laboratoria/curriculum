cd8ea8933389b67a86d183e6470018c6b16f4000
Lupo Montero <lupomontero@gmail.com>
2021-05-27 14:24:57 -0500

    chore(deps): Actualiza dependencias y borra archivos obsoletos

--- topics/javascript/05-objects/gulpfile.js
+++
@@ -1,8 +0,0 @@
-const gulp = require('gulp');
-const gulpConf = require('../gulp-utils.js');
-
-const lessons = [
-  '01-objects',
-];
-
-gulpConf(gulp, '05-objects', lessons);
--- topics/javascript/06-strings/gulpfile.js
+++
@@ -1,8 +0,0 @@
-const gulp = require('gulp');
-const gulpConf = require('../gulp-utils.js');
-
-const lessons = [
-  '01-strings',
-];
-
-gulpConf(gulp, '06-strings', lessons);
