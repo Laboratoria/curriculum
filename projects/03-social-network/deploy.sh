864a3d20a12d6d62e0d30660bd02a9b943d168d8
ivy <unjust@gmail.com>
2022-10-24 20:32:11 -0500

    add example deploy script

---
+++ projects/03-social-network/deploy.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env sh
+
+# usa este deploy script si estas desplegando tu static site a github pages o custom domain
+# mas info: https://vitejs.dev/guide/static-deploy.html
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# place .nojekyll to bypass Jekyll processing
+echo > .nojekyll
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git checkout -B main
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+
+echo "hello git"
+
+# si estas desplegando a https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+# tambien en vite.config.js agrega el propiedad "base" con el valor de tu <REPO> 
+# https://vitejs.dev/guide/static-deploy.html#github-pages
+
+cd -
---
+++ projects/03-social-network/vite.config.js
@@ -0,0 +1,24 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { defineConfig } from 'vite';
+
+export default defineConfig(({ command }) => {
+  if (command === 'build') {
+    return {
+      root: 'src',
+      // para hacer un deploy, configura propiedad base con el nombre/url de tu repo
+      // https://vitejs.dev/guide/static-deploy.html#github-pages
+      // ejemplo:
+      // base: '/social-network/',
+      build: {
+        minify: false,
+        rollupOptions: {
+          output: {
+            dir: './dist',
+          },
+        },
+        // sourcemap: true,
+      },
+    };
+  }
+  return {};
+});
