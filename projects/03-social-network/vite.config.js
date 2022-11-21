c96a5b3f732f526643908690872bf138ba9cb995
ivy <unjust@gmail.com>
2022-11-21 16:08:35 -0500

    agrega comentario a vite.config

--- projects/03-social-network/vite.config.js
+++ projects/03-social-network/vite.config.js
@@ -1,6 +1,8 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig } from 'vite';
 
+// https://vitejs.dev/config/#conditional-config
+
 export default defineConfig(({ command }) => {
   if (command === 'build') {
     return {
@@ -8,8 +10,7 @@ export default defineConfig(({ command }) => {
       // para hacer un deploy, configura propiedad base con el nombre/url de tu
       // para fazer uma implantação, defina a propriedade base para o nome/url do seu repositório
       // https://vitejs.dev/guide/static-deploy.html#github-pages
-      // ejemplo:
-      // base: '/social-network/',
+      // ejemplo base: '/social-network/',
       build: {
         minify: false,
         rollupOptions: {
