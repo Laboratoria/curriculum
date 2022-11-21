00cd93d80d2a03c25f6bbc90d979e8d358382d30
ivy <unjust@gmail.com>
2022-11-21 16:03:18 -0500

    agrega comentario en pt, quita sourcemap

--- projects/03-social-network/deploy.sh
+++ projects/03-social-network/deploy.sh
@@ -2,6 +2,12 @@
 
 # usa este deploy script si estas desplegando tu static site a github pages o custom domain
 # mas info: https://vitejs.dev/guide/static-deploy.html
+# https://vitejs.dev/guide/static-deploy.html#github-pages
+
+# use este script de implantação se estiver implantando seu site estático nas páginas
+# do github ou domínio personalizado
+# mais info: https://vitejs.dev/guide/static-deploy.html
+# https://vitejs.dev/guide/static-deploy.html#github-pages
 
 # abort on errors
 set -e
@@ -15,7 +21,8 @@ cd dist
 # place .nojekyll to bypass Jekyll processing
 echo > .nojekyll
 
-# if you are deploying to a custom domain
+# si estas desplegando a un dominio personalizado
+# se você estiver implantando em um domínio personalizado
 # echo 'www.example.com' > CNAME
 
 git init
@@ -23,12 +30,14 @@ git checkout -B main
 git add -A
 git commit -m 'deploy'
 
-# if you are deploying to https://<USERNAME>.github.io
+# si estas desplegando a https://<USERNAME>.github.io
+# se você estiver implantando em https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # si estas desplegando a https://<USERNAME>.github.io/<REPO>
+# en vite.config.js agrega el propiedad "base" con el valor de tu <REPO> 
+# se você estiver implantando em https://<USERNAME>.github.io/<REPO>
+# em vite.config.js adicione a propriedade "base" com o valor do seu <REPO> 
 # git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
-# tambien en vite.config.js agrega el propiedad "base" con el valor de tu <REPO> 
-# https://vitejs.dev/guide/static-deploy.html#github-pages
 
 cd -
--- projects/03-social-network/vite.config.js
+++ projects/03-social-network/vite.config.js
@@ -5,7 +5,8 @@ export default defineConfig(({ command }) => {
   if (command === 'build') {
     return {
       root: 'src',
-      // para hacer un deploy, configura propiedad base con el nombre/url de tu repo
+      // para hacer un deploy, configura propiedad base con el nombre/url de tu
+      // para fazer uma implantação, defina a propriedade base para o nome/url do seu repositório
       // https://vitejs.dev/guide/static-deploy.html#github-pages
       // ejemplo:
       // base: '/social-network/',
@@ -16,7 +17,6 @@ export default defineConfig(({ command }) => {
             dir: './dist',
           },
         },
-        // sourcemap: true,
       },
     };
   }
