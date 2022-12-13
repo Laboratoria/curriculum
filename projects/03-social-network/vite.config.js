f1b0e8c24cff3a0ba5010fa78a5f069cae8a3fbc
ivy <unjust@gmail.com>
2022-12-13 17:12:12 -0500

    agrega comenatrio a config

--- projects/03-social-network/vite.config.js
+++ projects/03-social-network/vite.config.js
@@ -1,14 +1,18 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig } from 'vite';
 
+// additional config to direct the vite build to src directory
 // https://vitejs.dev/config/#conditional-config
+// https://vitejs.dev/config/shared-options.html#root
 
 export default defineConfig(({ command }) => {
   if (command === 'build') {
     return {
       root: 'src',
-      // para hacer un deploy, configura propiedad base con el nombre/url de tu
-      // para fazer uma implantação, defina a propriedade base para o nome/url do seu repositório
+      // para hacer un deploy en github pages, configura propiedad base con el
+      // nombre/url de tu repo
+      // para fazer uma implantação github pages, defina a propriedade base
+      // para o nome/url do seu repositório
       // https://vitejs.dev/guide/static-deploy.html#github-pages
       // ejemplo base: '/social-network/',
       build: {
