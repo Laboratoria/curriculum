02ae3efa10d892bc51f397244cde12cb9be3ef9a
Lupo Montero <lupomontero@gmail.com>
2023-02-14 14:25:01 +0100

    feat(workers): Cambia workers por scripts de @laboratoria/react

--- public/workers/highlighter.js
+++ public/workers/highlighter.js
@@ -1,10 +1,14 @@
-self.addEventListener('message', (event) => {
-  const { version, code, language } = event.data;
-
+self.addEventListener('message', event => {
+  const {
+    version,
+    code,
+    language
+  } = event.data;
   importScripts(`//cdnjs.cloudflare.com/ajax/libs/highlight.js/${version}/highlight.min.js`);
-
   if (language) {
-    return postMessage(hljs.highlight(code, { language }).value);
+    return postMessage(hljs.highlight(code, {
+      language
+    }).value);
   }
   return postMessage(hljs.highlightAuto(code).value);
 });
