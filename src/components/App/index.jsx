a166ea9f1ce47668d5f9fc28d0d176c49e166267
Lupo Montero <lupomontero@gmail.com>
2023-03-01 23:22:43 +0100

    fix(App): Detecta idioma de usuaria si está presente en auth

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,47 +1,44 @@
 import { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { useApp } from '@laboratoria/react';
-import Loading from '../Loading';
+import { useApp, Loading } from '@laboratoria/react';
 import RoutesWithIntl from './RoutesWithIntl';
 import './App.css';
 
 const App = () => {
   const { auth } = useApp();
-  const [defaulLocale, setDefaulLocale] = useState();
+  const [defaultLang, setDefaultLang] = useState();
 
   useEffect(() => {
-    const userLang = auth.user?.lang;
-    setDefaulLocale(
-      userLang === 'es'
-        ? 'es-ES'
-        : userLang === 'pt'
-          ? 'pt-BR'
-          : navigator.language.split('-')[0] === 'pt'
-            ? 'pt-BR'
-            : 'es-ES'
+    if (auth.user && !auth.profile) {
+      return;
+    }
+
+    const profileLang = auth.user?.lang;
+    setDefaultLang(
+      ['es', 'pt'].includes(profileLang)
+        ? profileLang
+        : navigator.language.split('-')[0] === 'pt'
+          ? 'pt'
+          : 'es'
     )
   }, [auth]);
 
-  if (!defaulLocale) {
+  if (!defaultLang) {
     return <Loading />;
   }
 
-  const defaultLang = defaulLocale.split('-')[0];
-
   return (
     <Router>
-      <div className="App">
-        <Routes>
-          <Route
-            path="/"
-            element={<Navigate to={`/${defaultLang}`} />}
-          />
-          <Route
-            path="/:lang/*"
-            element={<RoutesWithIntl defaultLang={defaultLang} />}
-          />
-        </Routes>
-      </div>
+      <Routes>
+        <Route
+          path="/"
+          element={<Navigate to={`/${defaultLang}`} />}
+        />
+        <Route
+          path="/:lang/*"
+          element={<RoutesWithIntl defaultLang={defaultLang} />}
+        />
+      </Routes>
     </Router>
   );
 };
