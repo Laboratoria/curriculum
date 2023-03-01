b36d554616cac735a7a436656a53d54f801e8b3b
Lupo Montero <lupomontero@gmail.com>
2023-03-02 00:51:11 +0100

    fix(App): Corrige redirección a inicio (incluye trailing slash)

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -32,7 +32,7 @@ const App = () => {
       <Routes>
         <Route
           path="/"
-          element={<Navigate to={`/${defaultLang}`} />}
+          element={<Navigate to={`/${defaultLang}/`} />}
         />
         <Route
           path="/:lang/*"
