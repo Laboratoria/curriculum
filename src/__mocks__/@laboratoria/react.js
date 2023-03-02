5b0b4ae9720ba2e9f5522915a09ef5755da77996
Lupo Montero <lupomontero@gmail.com>
2023-03-02 04:33:29 +0100

    fix(tests): Arregla pruebas después de actualizar dependencias

--- src/__mocks__/@laboratoria/react.js
+++ src/__mocks__/@laboratoria/react.js
@@ -2,6 +2,11 @@ export const useApp = jest.fn();
 
 export const themeConfig = {
   palette: {
+    primary: { main: '#FFE521' },
     mint: {},
+    magenta: {},
   },
 };
+
+export const Loading = () => <>Loading...</>;
+export const ScrollRestoration = () => null;
