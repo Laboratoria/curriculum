e918faf1ed2d0928fc8c488d2cbfbb50efc0fe33
Ivy Feraco <ivy@laboratoria.la>
2023-05-17 09:39:52 -0500

    refactor(content): bq api - codigo necesario por mongoDB setup, mover docker reqs, fix spawn en Windows (#1353)
    * trabajo necesario por mongoDB setup y spawn en Windows
    
    * cambios a README
    
    * ideas para adiciones GETTING_STARTED
    
    * comentando config y e2e tests mejor
    
    * mdlint fixes
    
    * separating docker and mongo guides
    
    * remove mongo deps from package.json
    
    * clean mongoDB references de proyecto
    
    * patch for Mongo
    
    * Update projects/04-burger-queen-api/README.md
    
    * cambios al readme y guia
    
    * mdlint fixes
    
    * readd jsonwebtoken dep
    
    * remove ws from patch
    
    * patch in the wrong place
    
    * patch mongoDB created from project not from bootcamp
    
    * actualizar table of contents
    
    * update guia and remove mongoose refs
    
    * minor corrections
    
    * rehacer el patch
    
    * corrections
    
    * remove redundant checklist, node driver resource
    
    * Update TODO projects/04-burger-queen-api/controller/users.js
    
    Co-authored-by: Alfredo González <12631491+mfdebian@users.noreply.github.com>
    
    * mas detalle en TODO comentarios
    
    * quitar mysql postgres de readme inicial, hacker edition docker
    
    * apply and remove patch for e2e tests
    
    * linter and section for hacker edition
    
    * Apply grammar and spelling suggestions from code review
    
    Co-authored-by: Mariano Crowe <merunga@gmail.com>
    
    * remove MySQL and add code link for snippet
    
    * mas correciones de spelling, con uso de extension
    
    * Update auth.js
    
    * Apply suggestions from code review
    
    * update pt README
    
    * translations in pt, editions of docker readme
    
    * borra parte 1 de docker, es redundante
    
    * mdlint fixes
    
    * cambios de connect y TODOs
    
    * agregamos status de resp en globalSetup errors
    
    * Update projects/04-burger-queen-api/guides/GETTING-STARTED-MONGODB.md
    
    Co-authored-by: Alfredo González <12631491+mfdebian@users.noreply.github.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Alfredo González <12631491+mfdebian@users.noreply.github.com>
    
    * espanol no pt, error response
    
    * Update README.md guides url
    
    * Update README.pt.md url guides y texto duplicado
    
    ---------
    
    Co-authored-by: Alfredo González <12631491+mfdebian@users.noreply.github.com>
    Co-authored-by: Mariano Crowe <merunga@gmail.com>

---
+++ projects/04-burger-queen-api/connect.js
@@ -0,0 +1,10 @@
+const config = require('./config');
+
+// eslint-disable-next-line no-unused-vars
+const { dbUrl } = config;
+
+async function connect() {
+  // TODO: Conexión a la Base de Datos
+}
+
+module.exports = { connect };
--- projects/04-burger-queen-api/controller/users.js
+++ projects/04-burger-queen-api/controller/users.js
@@ -1,4 +1,5 @@
 module.exports = {
   getUsers: (req, resp, next) => {
+    // TODO: Implementa la función necesaria para traer la colección `users`
   },
 };
