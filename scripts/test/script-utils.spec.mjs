f884f1e70cc9be55a5c91ae3d92b171b41dbc904
ivy <unjust@gmail.com>
2023-07-14 17:22:43 -0500

    adding tests for the heluvit

---
+++ scripts/test/script-utils.spec.mjs
@@ -0,0 +1,20 @@
+import { getFilesWithLocales } from '../script-utils.mjs';
+import { copy } from '../create-cohort-project.mjs';
+import { assert } from 'chai';
+
+describe('script utils', () => {
+  
+  describe('getFilesWithLocale', () => {
+    it('should return all files with .pt.md when locale is pt', () => {
+      const filesPt = getFilesWithLocales('/Users/ivy/Work/Laboratoria/repos/dev/bootcamp/scripts/test/fixtures/project-with-localized-docs', ['pt']);
+      assert.equal(filesPt.length, 2);
+      assert.equal(filesPt.filter(f => !f.endsWith('.pt.md')).length, 0);
+    });
+  
+    it('should return all files with supported locales jp, pt', () => {
+      const filesPt = getFilesWithLocales('/Users/ivy/Work/Laboratoria/repos/dev/bootcamp/scripts/test/fixtures/project-with-localized-docs', ['jp', 'pt']);
+      assert.equal(filesPt.length, 3);
+      assert.equal(filesPt.filter(f => !(f.endsWith('.pt.md') || f.endsWith('.jp.md'))).length, 0);
+    });
+  })
+});
