4c14401973548e2e7ffe73ebf1dc405a874c03e8
adolivaresl <domioli@gmail.com>
2023-07-25 16:29:46 -0400

    feature(Sentry): Agrega Sentry para manejo de errores

--- src/index.jsx
+++ src/index.jsx
@@ -8,8 +8,37 @@ import {
 import CssBaseline from '@mui/material/CssBaseline';
 import { AppProvider, themeConfig } from '@laboratoria/react';
 import '@laboratoria/react/src/theme/index.css';
+import * as Sentry from "@sentry/react";
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+
+Sentry.init({
+  environment: (
+    process.env.NODE_ENV !== 'production'
+      ? 'development'
+      : window.location.hostname === 'curriculum.laboratoria.la'
+        ? 'production'
+        : 'staging'
+  ),
+  release: process.env.REACT_APP_VERSION,
+  dsn: process.env.REACT_APP_SENTRY_DSN,
+  integrations: [
+    new Sentry.BrowserTracing({
+      tracePropagationTargets: [
+        "localhost",
+        /^https:\/\/api\.laboratoria\.la/,
+      ],
+    }),
+    new Sentry.Replay(),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
+
+const AppWithProfiler = Sentry.withProfiler(App);
 
 ReactDOM.render(
   <React.StrictMode>
@@ -17,7 +46,7 @@ ReactDOM.render(
       <ThemeProvider theme={createTheme(themeConfig)}>
         <AppProvider>
           <CssBaseline />
-          <App />
+          <AppWithProfiler />
         </AppProvider>
       </ThemeProvider>
     </StyledEngineProvider>
