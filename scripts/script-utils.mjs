f0d2f0c6be841d17f2fbdd165d2a9fbc1d4e9422
Ivy Feraco <ivy@laboratoria.la>
2023-08-07 18:33:12 -0500

    fix(script): Cambia `copy` para solo copiar archivos de idioma elegido (fixes #1425)
    * fix(script): primer intenta a cambiar copy pero solo apoya pt o es
    
    * fix(script): refactoring files functions and starting tests
    
    * getFilesWithLocales, moving to script utils
    
    * adding tests for the heluvit
    
    * refactor with reduce, correct path in test
    
    * remove conditional
    
    * small correction in test
    
    * filter out chosen local, return Promise
    
    * correct filepath concat and add case to tests
    
    * Update scripts/script-utils.mjs
    
    Co-authored-by: Alfredo González <12631491+mfdebian@users.noreply.github.com>
    
    * resolve instead of join, remove reduntant repodir
    
    ---------
    
    Co-authored-by: Alfredo González <12631491+mfdebian@users.noreply.github.com>

---
+++ scripts/script-utils.mjs
@@ -0,0 +1,16 @@
+import { readdirSync } from 'node:fs';
+import path from 'node:path';
+
+export const defaultLocale = 'es';
+export const supportedLocales = ['es', 'pt'];
+
+export const getFilesWithLocales = (dir, locales) => {
+  const dirents = readdirSync(dir, { withFileTypes: true });
+  const filesWithExt = dirents.reduce((accumFiles, dirent) => {
+    const fullPath = path.resolve(dir, dirent.name);
+    dirent.isDirectory() ? accumFiles.push(...getFilesWithLocales(fullPath, locales)) : accumFiles.push(fullPath); 
+    return accumFiles;
+  }, []);
+  const regexLocaleMD = new RegExp(`\.(${locales.join('|')})\.md`);
+  return filesWithExt.filter((filepath) => filepath.match(regexLocaleMD));
+}
