6b94a5b7da78c315ffc8951bf05846d21190304f
ivy <unjust@gmail.com>
2023-08-17 18:04:18 -0500

    renderitems

--- projects/02-data-lovers/tests-read-only/oa/oa-html.spec.js
+++ projects/02-data-lovers/tests-read-only/oa/oa-html.spec.js
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
 */
 import fs from 'fs';
-import { renderView } from '../../src/viewFunctions.js';
+import { renderItems } from '../../src/viewFunctions.js';
 const html = fs.readFileSync('./src/index.html', 'utf-8');
 document.body.innerHTML = html;
 const fakeData = [
