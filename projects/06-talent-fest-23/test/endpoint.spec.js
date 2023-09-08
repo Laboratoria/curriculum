8fc7b0d87195deadc21b3c76a4d3269b805c3267
Daniel Gutierrez <cdanielgl@gmail.com>
2023-09-07 19:14:55 -0600

    actualiza test para reto 1

--- projects/06-talent-fest-23/test/endpoint.spec.js
+++ projects/06-talent-fest-23/test/endpoint.spec.js
@@ -1,52 +1,38 @@
-const endpoint =
-  "http://localhost:3000/api/v1/prediction/2429f869-6ca6-4d30-bf10-0617c3ce49a9";
+const flow = require('./flow.json');
 
-async function query(data) {
-  const response = await fetch(endpoint, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
+describe('Valida el archivo JSON del flow', () => {
+  test('Debe contener al menos 1 nodo del tipo TextSplitter', async () => {
+    expect(
+      flow.nodes.some((node) => node.data.type.includes('TextSplitter'))
+    ).toBe(true);
   });
-  const result = await response.json();
-  return result;
-}
-
-describe("test endpoint responses", () => {
-  test("Debe incluir Descroisilles para el creador de la cafetera", async () => {
-    const response = await query({
-      history: [],
-      overrideConfig: {},
-      question: "¿Quién fue el inventor del primer modelo de cafetera?",
-    });
-    expect(response.toLowerCase()).toContain("descroisilles");
+  test('Debe hacer uso de Embeddings', async () => {
+    expect(
+      flow.nodes.some((node) => node.data.category.includes('Embeddings'))
+    ).toBe(true);
   });
-
-  test("Debe responder Melitta Bentz", async () => {
-    const response = await query({
-      history: [],
-      overrideConfig: {},
-      question: "¿Cómo se llamaba la primera mujer interesada en el invento?",
-    });
-    expect(response).toContain("Melitta Bentz");
+  test('Debe utilizar el modelo de chat gpt-3.5-turbo', async () => {
+    expect(
+      flow.nodes.some(
+        (node) =>
+          node.data.type === 'ChatOpenAI' &&
+          node.data.inputs.modelName === 'gpt-3.5-turbo'
+      )
+    ).toBe(true);
   });
-
-  test("Debe incluir Descroisilles para el creador de la cafetera", async () => {
-    const response = await query({
-      history: [],
-      overrideConfig: {},
-      question: "¿Cuál es la nacionalidad de Melitta?",
-    });
-    expect(response.toLowerCase()).toContain("alemana");
+  test('Debe hacer uso de una Vectore Store', async () => {
+    expect(
+      flow.nodes.some((node) => node.data.category === 'Vector Stores')
+    ).toBe(true);
   });
-});
-
-describe("Valida el archivo JSON del flow", () => {
-  test("find Text File node", async () => {
-    const flow = require("./flow.json");
-    expect(flow.nodes.some((node) => node.data.label === "Text File")).toBe(
-      true
-    );
+  test('Debe cargar al menos 1 Documento', async () => {
+    expect(flow.nodes.some((node) => node.data.type === 'Document')).toBe(true);
+  });
+  test('Debe utilizar el nodo Conversational Retrieval QA Chain', async () => {
+    expect(
+      flow.nodes.some(
+        (node) => node.data.type === 'ConversationalRetrievalQAChain'
+      )
+    ).toBe(true);
   });
 });
