dba14ceec5b4e6bc48ea64fe64ad54b978dd36c0
Christian R. Lara <christianramirezlara1@gmail.com>
2023-10-11 10:03:43 -0600

    Modify boilerplate based on comments from unjust

--- projects/03-dataverse-chat/src/app.js
+++ projects/03-dataverse-chat/src/app.js
@@ -1,2 +1,19 @@
-// Aquí definirás tus rutas e importarás los componentes que vas a renderizar.
+// En este archivo definirás tus rutas e importarás los componentes que vas a renderizar.
 
+/*
+import Example from './views/Example/index.js';
+
+Ejemplo de definición de rutas:
+
+const routes = {
+    "/": Example,
+    ...
+}
+*/
+
+/*
+TODO:
+1.- Definir rutas en router.
+2.- Pasar "root element" a router.
+3.- Invocar el router para renderizar la vista correcta.
+*/
