6381ac8050303f2ee87667fb35776fa8bb2e9553
Christian R. Lara <christianramirezlara1@gmail.com>
2023-10-11 11:33:18 -0600

    Change app.js to index.js and use the simple structure for Example component

--- projects/03-dataverse-chat/src/index.js
+++ projects/03-dataverse-chat/src/index.js
@@ -1,7 +1,7 @@
 // En este archivo definirás tus rutas e importarás los componentes que vas a renderizar.
 
 /*
-import Example from './views/Example/index.js';
+import Example from './views/Example.js';
 
 Ejemplo de definición de rutas:
 
